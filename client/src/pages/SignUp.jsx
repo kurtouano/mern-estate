bdc4cbc7a7e1119a12524c31670785c57133eea4
kurtouano <kurt0216@gmail.com>
2025-04-28 15:41:26 +0800

    fixed folder manangement and created new mongodb dotenv connection

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div>Signup</div>
+    <div className='p-3 max-w-md mx-auto'>
+      <h1 className='text-3xl text-center font-semibold mt-10 mb-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input type='text' placeholder='Username' className='p-3 rounded-lg'/> 
+        <input type='email' placeholder='Email' className='p-3 rounded-lg'/> 
+        <input type='password' placeholder='Password' className='p-3 rounded-lg'/> 
+        <button className='bg-DarkBlue text-OffWhite p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
+      </form>
+      <div className='flex flex-row gap-2 my-3'>
+        <text>Have an account? </text>
+        <Link to='/sign-in'>
+          <text className='text-Cyan font-semibold underline'>Sign In</text>
+        </Link>
+      </div>
+    
+    </div>
+    
   )
 }
