fc99881b9b1dd86d407a10c81dead73142ad0bae
kurtouano <kurt0216@gmail.com>
2025-05-08 19:05:08 +0800

    Added error handling and disply in sign up

--- client/src/components/Auth/SignUp/SignUp.jsx
+++ client/src/components/Auth/SignUp/SignUp.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 import "../shared/AuthForm.css";
 import { RxEyeClosed, RxEyeOpen} from "react-icons/rx";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
 
 export default function SignUpComponent() {
+    const navigate = useNavigate();
     const [showPassword, setShowPassword] = useState(false);
     const [formData, setFormData] = useState({});
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     const togglePasswordVisibility = () => {
         setShowPassword(!showPassword);
@@ -23,15 +27,32 @@ export default function SignUpComponent() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const res = await fetch(`${API_URL}/api/auth/sign-up`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formData),
-        });
-        const data = await res.json();
-        console.log(data);
+        setLoading(true);
+
+        try {
+            const res = await fetch(`${API_URL}/api/auth/sign-up`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+
+            if (data.success == false) {
+                setError(data.message);
+                setLoading(false);
+                return;
+            }
+            setLoading(false);
+            setError(null);
+            navigate('/sign-in');
+        } 
+        catch (error) {
+            setLoading(false);
+            setError(error.message);
+            return;
+        }
     }
 
   return (
@@ -39,6 +60,7 @@ export default function SignUpComponent() {
         <main className="sign-in-container">
             
             <h1>Create an account</h1>
+            {error && <section className="sign-in-error-container">{error}</section>}
             <form onSubmit={handleSubmit}>
                 <div className="sign-in-input">
                     <label htmlFor="username">Username</label>
@@ -61,7 +83,7 @@ export default function SignUpComponent() {
                     <a href="" className="sign-in-forgot-pass">Forgot password?</a>
                 </div>
 
-                <button type="submit" className="sign-in-submit-btn">Sign up</button>
+                <button disabled={loading} type="submit" className="sign-in-submit-btn">{loading ? "Loading..." : "Sign up"}</button>
                 <button type="submit" className="sign-in-google-btn"><FcGoogle className="sign-in-google-icon"/>Continue with Google</button>
                 <p className="sign-in-have-acc">Have an account? <Link to="/sign-in">Sign in</Link></p>
             </form>
