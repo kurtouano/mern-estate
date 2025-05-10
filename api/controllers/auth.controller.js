16f8cea20443b691752a63170ba8e6ef6a7dedff
kurtouano <kurt0216@gmail.com>
2025-05-10 22:52:11 +0800

    added jwt token authorization, and optimized sign in backend

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"; // Importing the User model from the user.model.js file
 import bcrypt from "bcryptjs"; // Importing bcrypt to hash the password
+import jwt from "jsonwebtoken"; // Importing jsonwebtoken to create a token
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
@@ -14,7 +15,8 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error); // Send error to the error handling middleware
     // next(errorHandler(500, "Internal Server Error")); // This is another way to send a customized error message
-  }
+  } 
+  
 };
 
 export const signin = async (req, res, next) => {
@@ -24,7 +26,13 @@ export const signin = async (req, res, next) => {
     const validUser = await User.findOne({ email }); // Find the user in the DB by email
     if (!validUser) return next(errorHandler(404, "User not found")); 
     const validPassword = bcrypt.compareSync(password, validUser.password); // Compare the password with the hashed password in the DB 
-    if (!validPassword) return next(errorHandler(400, "Incorrect Credentials!")); // If the password is wrong, send an error message
+    if (!validPassword) return next(errorHandler(400, "Incorrect Password.")); // If the password is wrong, send an error message
+    const { password: _, ...user } = validUser._doc; // Destructure the user object to remove the password field
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET); // Create a token with the user ID and secret key
+    res
+      .cookie("access_token", token, { httpOnly: true }) 
+      .status(200)
+      .json(user); // Send a response to the user
   } catch (error) {
     next(error); // Send error to the error handling middleware
   }
--- api/utils/error.js
+++ api/utils/error.js
@@ -1,3 +1,5 @@
+// This is a custom error handler function that takes a status code and a message as parameters
+
 export const errorHandler = (statusCode, message) => {
     const error = new Error();
     error.statusCode = statusCode;
